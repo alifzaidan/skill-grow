a2c2c7dec5b0c75f43913a62a0988fd1229103a9
alifzaidan <zaidanalif22710@gmail.com>
2025-12-26 13:15:27 +0700

    update build

--- public/build/assets/input-error-G_U02IKo.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CvxuV3NM.js";import{c as o}from"./utils-PdpvZsej.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
