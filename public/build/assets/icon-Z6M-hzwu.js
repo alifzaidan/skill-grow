ecac492b05f0aee6b353b1f3173369eddfe1d81d
alifzaidan <zaidanalif22710@gmail.com>
2025-12-31 11:24:38 +0700

    update build

---
+++ public/build/assets/icon-Z6M-hzwu.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BHncSzdJ.js";import{c as t}from"./utils-DmF5OAjJ.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
