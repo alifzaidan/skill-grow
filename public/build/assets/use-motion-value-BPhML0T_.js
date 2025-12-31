bd55dfe33dec8ee1df9e11d3bed196da5ea5b0df
alifzaidan <zaidanalif22710@gmail.com>
2025-12-31 10:04:08 +0700

    update build

---
+++ public/build/assets/use-motion-value-BPhML0T_.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-D4FsfSBA.js";import{u as a,M as u,c as r}from"./proxy-UQHLnWiC.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
