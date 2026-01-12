dee80b1788b50f811c45d3505982fa17fac98489
alifzaidan <zaidanalif22710@gmail.com>
2026-01-12 15:09:40 +0700

    update build

--- public/build/assets/icon-BOwItDgn.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-12w6AqM-.js";import{c as t}from"./utils-D3sAJBvS.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
