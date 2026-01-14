e0bf365368b7f95cd076b5f0dfa3242030786f62
alifzaidan <zaidanalif22710@gmail.com>
2026-01-14 10:28:50 +0700

    update build

--- public/build/assets/icon-DBCMX58i.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DdB2A5Dw.js";import{c as t}from"./utils-BT0-AaAZ.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
--- public/build/assets/skeleton-CBcBNc_o.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-DdB2A5Dw.js";import{c as r}from"./utils-BT0-AaAZ.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
