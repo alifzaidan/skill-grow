357484574a82da0d8e63232d0821d01f7918b273
alifzaidan <zaidanalif22710@gmail.com>
2026-02-26 09:45:38 +0700

    update build

---
+++ public/build/assets/input-error-BVe91V71.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-_U5oHNeV.js";import{c as o}from"./utils-BdeRMn5h.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
