f1d5f1d29978f67b04ec0d5a4edcf47ed421da31
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-03-07 11:36:54 +0700

    build

---
+++ public/build/assets/skeleton-DRXq2tQi.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-8PhLnNH9.js";import{c as r}from"./utils-C5awvQPI.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
--- public/build/assets/skeleton-DTm_VlLa.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-CPPnGTOM.js";import{c as r}from"./utils-BuAZDDMu.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
