4829bc3b0b24b9b8a82829604add9fe85a83c90d
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-03-14 14:29:47 +0700

    build

--- public/build/assets/use-motion-value-aaYC4vYk.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-GK7h16pJ.js";import{u as a,M as u,c as r}from"./proxy-CSr1hlNI.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
