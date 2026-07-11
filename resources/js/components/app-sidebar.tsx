d542b54385cef5b397e28d63af8a16f66c7da25b
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-07-11 15:37:05 +0700

    merge

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -135,24 +135,6 @@ const allNavItems: (NavItem & { roles: string[] })[] = [
         roles: ['admin'],
     },
     {
-        title: 'Sertifikat',
-        href: '/admin/certificates',
-        icon: Dock,
-        roles: ['admin'],
-    },
-    {
-        title: 'Kode Diskon',
-        href: '/admin/discount-codes',
-        icon: Banknote,
-        roles: ['admin'],
-    },
-    {
-        title: 'Flyer Promosi',
-        href: '/admin/promotions',
-        icon: Proportions,
-        roles: ['admin'],
-    },
-    {
         title: 'Transaksi',
         href: '/admin/transactions',
         icon: DollarSign,
