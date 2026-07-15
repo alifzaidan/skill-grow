0bee0c2ce91f02037b43df0aaedc88eb5895f975
alifzaidan <zaidanalif22710@gmail.com>
2026-07-15 13:46:18 +0700

    update build

--- public/build/assets/skeleton-3ropSL99.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BnCVXAuq.js";import{c as r}from"./utils-Bot_ey0u.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
