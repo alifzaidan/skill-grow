ad379572c5ad90718b4b1e0025b83b59b73bc36f
alifzaidan <zaidanalif22710@gmail.com>
2026-08-04 10:18:12 +0700

    update build

---
+++ public/build/assets/index-CpwTkdFm.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-sf53NqeM.js";var t=a();const e=r(t);export{e as R,t as r};
--- public/build/assets/index-tpnoMFJS.js
+++
@@ -1,1 +0,0 @@
-import{g as r,a}from"./app-DjTNO7mC.js";var t=a();const e=r(t);export{e as R,t as r};
