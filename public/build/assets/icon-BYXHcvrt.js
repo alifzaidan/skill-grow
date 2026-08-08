8f263ff3635d99a2745c248cabee1f038c7669d3
alifzaidan <zaidanalif22710@gmail.com>
2026-08-08 10:55:53 +0700

    feat: add WhatsApp share and Instagram story verification photos to scholarship applications

--- public/build/assets/icon-BYXHcvrt.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-X9VVDhux.js";import{c as t}from"./utils-B9WzKQ83.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
--- public/build/assets/skeleton-DDWcwEGy.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-X9VVDhux.js";import{c as r}from"./utils-B9WzKQ83.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
