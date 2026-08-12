f5031b25d7ca8e365e71ce72381a2153b739f406
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-12 11:06:11 +0700

    update build

---
+++ public/build/assets/use-motion-value-ByGrZGQ8.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-DTD2vU3L.js";import{u as a,M as u,c as r}from"./proxy-DfB3m215.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
