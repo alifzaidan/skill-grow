8bc4e958705b5b31315fcb8a5658badb3218498a
alifzaidan <zaidanalif22710@gmail.com>
2026-08-24 18:01:18 +0700

    update build

---
+++ public/build/assets/input-error-VvEJkQaR.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B41EgYqq.js";import{c as o}from"./utils-DQ7CP89L.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
---
+++ public/build/assets/skeleton-BgJDHyn3.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-B41EgYqq.js";import{c as r}from"./utils-DQ7CP89L.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
